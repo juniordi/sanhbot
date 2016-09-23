f67c0af41c7b49e251638b6f16a4fbfca3990484
juniordi <lehoangdieu@gmail.com>
2016-09-23 16:20:21 +0700

    the 5th

--- index.js
+++ index.js
@@ -365,7 +365,7 @@ function receivedPostback(event) {
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
-  sendTextMessage(senderID, a_catalogue[payload]["answer"]);
+  sendTextMessage(senderID, payload);
 }
 
 function sendTextMessage(recipientId, messageText) {
